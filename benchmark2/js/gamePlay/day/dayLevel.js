5e7085e30b36dbcddecb5c3fa5b243797e030efd
My Name <my-name@chromium.org>
2019-04-10 13:47:59 -0400

    added some states
    ye boi

--- benchmark2/js/gamePlay/day/dayLevel.js
+++ benchmark2/js/gamePlay/day/dayLevel.js
@@ -1,1 +1,27 @@
 //Day time level
+'use strict'
+
+class dayLevel{
+    constructor(){}
+
+    
+
+
+
+}
+
+
+var time;
+var map;
+var monsterArray;
+
+
+
+
+
+
+
+
+
+
+
--- benchmark2/js/gamePlay/day/dayUI.js
+++ benchmark2/js/gamePlay/day/dayUI.js
@@ -1,1 +1,24 @@
 //User input class for daytime
+
+
+var TopDownGame = TopDownGame || {};
+
+TopDownGame.dayUI = function(){};
+
+//setting game configuration and loading the assets for the loading screen
+TopDownGame.Boot.prototype = {
+  preload: function() {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
