c88b3eb56e6bf3523e1db2482c783c50e3437834
songdavid98 <songdavid98@gmail.com>
2019-04-13 00:02:30 -0400

    added nightscene stuff

--- benchmark2/js/scenes/DayScene2.js
+++ benchmark2/js/scenes/DayScene2.js
@@ -1,22 +1,28 @@
 //Day time level 
 
-import {SCENES} from "../constants/SceneNames.js";
-import {HEROES} from "../constants/PlayerTypes.js";
-import {DayPlayer} from "../gamePlay/DayPlayer.js";
-export class DayScene extends Phaser.Scene{
-    constructor(){
+import {
+    SCENES
+} from "../constants/SceneNames.js";
+import {
+    HEROES
+} from "../constants/PlayerTypes.js";
+import {
+    DayPlayer
+} from "../gamePlay/DayPlayer.js";
+export class DayScene extends Phaser.Scene {
+    constructor() {
         super({
             key: SCENES.DAY
         })
     }
-    init(data){
+    init(data) {
         this.time;
         this.map;
         this.monsterArray;
         this.level = data.level;
         this.mapLevel;
     }
-    preload(){
+    preload() {
         this.load.image("terrain", "assets/images/tiles.png");
         //this.load.spritesheet("shieldHero", "assets/images/shieldHero1.png", {frameWidth: 32, frameHeight:32});
 
@@ -24,33 +30,33 @@ export class DayScene extends Phaser.Scene{
 
         //this.load.multiatlas('cityscene', 'assets/images/cityscene.json', "assets/images");
 
-        switch(this.level){
-            case 1: 
+        switch (this.level) {
+            case 1:
                 this.load.tilemapTiledJSON("map1", "assets/tilemaps/dungeon2.json");
                 this.mapLevel = "map1";
                 console.log("Welcome to level 1");
                 break;
-            case 2: 
+            case 2:
                 this.load.tilemapTiledJSON("map2", "assets/tilemaps/dungeon2.json");
                 this.mapLevel = "map2";
                 console.log("make suer this dungeon even exits, dumbo");
                 break;
-            case 3: 
+            case 3:
                 this.load.tilemapTiledJSON("map3", "assets/tilemaps/dungeon3.json");
                 this.mapLevel = "map3";
                 console.log("make suer this dungeon even exits, dumbo");
                 break;
-            case 4: 
+            case 4:
                 this.load.tilemapTiledJSON("map4", "assets/tilemaps/dungeon4.json");
                 this.mapLevel = "map4";
                 console.log("make suer this dungeon even exits, dumbo");
                 break;
-            case 5: 
+            case 5:
                 this.load.tilemapTiledJSON("map5", "assets/tilemaps/dungeon5.json");
                 this.mapLevel = "map5";
                 console.log("make suer this dungeon even exits, dumbo");
                 break;
-            case 6: 
+            case 6:
                 this.load.tilemapTiledJSON("map6", "assets/tilemaps/dungeon6.json");
                 this.mapLevel = "map6";
                 console.log("make suer this dungeon even exits, dumbo");
@@ -59,19 +65,19 @@ export class DayScene extends Phaser.Scene{
                 this.scene.start(SCENES.Main_MENU, "how");
         }
     }
-    create(){
+    create() {
         //Generate map
         this.map = this.add.tilemap(this.mapLevel);
         let terrain = this.map.addTilesetImage("tiles", "terrain");
-        this.baseLayer = this.map.createStaticLayer("base", [terrain], 0, 0).setScale(2,2);
-        this.wallLayer = this.map.createStaticLayer("walls", [terrain], 1, 0).setScale(2,2); 
+        this.baseLayer = this.map.createStaticLayer("base", [terrain], 0, 0).setScale(2, 2);
+        this.wallLayer = this.map.createStaticLayer("walls", [terrain], 1, 0).setScale(2, 2);
         //Generate sprite
-        this.sprite = this.physics.add.sprite(100,100, 'shieldHero').setScale(3,3);
+        this.sprite = this.physics.add.sprite(100, 100, 'shieldHero').setScale(3, 3);
         console.log(this.sprite);
         //this.walk = this.sprite.animations.add('walk'); 
-	//collisions
-	this.wallLayer.setCollisionBetween(265,300);
-	this.physics.add.collider(this.sprite,this.wallLayer);
+        //collisions
+        this.wallLayer.setCollisionBetween(265, 300);
+        this.physics.add.collider(this.sprite, this.wallLayer);
 
         //Create the heroes
         console.log(this.input.keyboard);
@@ -82,8 +88,16 @@ export class DayScene extends Phaser.Scene{
         this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);*/
         this.input.keyboard.addKeys('W,S,A,D');
 
-        this.shieldHero = new DayPlayer({"sprite":this.sprite,"physics":this.physics,"keyboard":this.input.keyboard,
-        "health":1,"attack":1,"speed":1,"playerType":HEROES.SHIELD_HERO, "anims":this.anims});
+        this.shieldHero = new DayPlayer({
+            "sprite": this.sprite,
+            "physics": this.physics,
+            "keyboard": this.input.keyboard,
+            "health": 1,
+            "attack": 1,
+            "speed": 1,
+            "playerType": HEROES.SHIELD_HERO,
+            "anims": this.anims
+        });
         this.shieldHero.create();
         //DayPlayer swordHero = new DayPlayer();
         //DayPlayer mageHero = new DayPlayer();
@@ -93,21 +107,9 @@ export class DayScene extends Phaser.Scene{
 
 
     }
-    update(time, delta){
+    update(time, delta) {
         this.shieldHero.update();
 
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
--- benchmark2/js/scenes/nightScene.js
+++ benchmark2/js/scenes/nightScene.js
@@ -1,5 +1,79 @@
-//Night time level
+import {
+    SCENES
+} from "../constants/SceneNames.js";
+export class NightScene extends Phaser.Scene {
+    constructor() {
+        super({
+            key: SCENES.NIGHT
+        })
+    }
+    init(data) {
+        console.log(data);
+        console.log("entered night");
+        this.time;
+        this.map;
+        this.monsterArray;
+        this.level = data.level;
+        this.mapLevel;
+
+        this.buttonYinc = 100;
+        this.buttonX = 200;
+    }
+
+    preload() {
+        this.load.image("terrain", "assets/images/tiles.png");
+
+        this.load.image("buyarrow", "assets/images/buttons/buyarrowturret.JPG");
+        this.load.image("buycannon", "assets/images/buttons/buycannonturret.JPG");
+        this.load.image("buywall", "assets/images/buttons/buywall.JPG");
+        this.load.image("startwave", "assets/images/buttons/startwave.JPG");
+
+        this.load.tilemapTiledJSON("map1", "assets/tilemaps/nightmap.json");
+        this.mapLevel = "map1";
+        console.log("Welcome to level " + this.level);
+
+
+    }
+
+    create() {
+        //Generate map
+        this.map = this.add.tilemap(this.mapLevel);
+        let terrain = this.map.addTilesetImage("tiles", "terrain");
+        this.groundLayer = this.map.createStaticLayer("background ground", [terrain], 0, 0).setScale(2, 2);
+        this.plantLayer = this.map.createStaticLayer("background plants", [terrain], 1, 0).setScale(2, 2);
+        this.wallLayer = this.map.createStaticLayer("background wall", [terrain], 2, 0).setScale(2, 2);
+
+        //create buttons
+        let startwave = this.add.image(this.buttonX, this.buttonYinc * 2, "startwave").setDepth(3).setScale(.5, .5);
+
+        let buywall = this.add.image(this.buttonX, this.buttonYinc * 3, "buywall").setDepth(3).setScale(.5, .5);
+
+        let buyarrow = this.add.image(this.buttonX, this.buttonYinc * 4, "buyarrow").setDepth(3).setScale(.5, .5);
+
+        let buycannon = this.add.image(this.buttonX, this.buttonYinc * 5, "buycannon").setDepth(3).setScale(.5, .5);
 
 
 
+        //add button events
+        startwave.setInteractive();
+        startwave.on("pointerdown", () => {
+            console.log("startwave pressed")
+        });
 
+        buywall.setInteractive();
+        buywall.on("pointerdown", () => {
+            console.log("buywall pressed")
+        });
+
+        buyarrow.setInteractive();
+        buyarrow.on("pointerdown", () => {
+            console.log("buyarray pressed")
+        });
+
+        buycannon.setInteractive();
+        buycannon.on("pointerdown", () => {
+            console.log("buycannon pressed")
+        });
+    }
+
+}
