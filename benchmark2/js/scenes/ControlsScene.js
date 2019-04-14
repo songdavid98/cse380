a607fa694c4d0271f225cf61b9809a3c35370a92
dumdo <43555901+dumdo@users.noreply.github.com>
2019-04-13 22:21:41 -0400

    added WIP controls screen

--- benchmark2/js/scenes/ControlsScene.js
+++ benchmark2/js/scenes/ControlsScene.js
@@ -15,6 +15,12 @@ export class ControlsScene extends Phaser.Scene{
         //let logo = this.add.image(this.game.renderer.width / 2, this.game.renderer.height*.35, "logo").setDepth(1).setScale(.5,.5);
         this.cameras.main.setBackgroundColor('#008080')
         let backButton = this.add.image(this.game.renderer.width*.05,this.game.renderer.height*.1,"backButton").setDepth(1).setScale(2,2);
+        let controlsBox = this.add.image(this.game.renderer.width*.5, this.game.renderer.height*.5,'blueBox').setDepth(1).setScale(4,2);
+        this.add.text(this.game.renderer.width*.42, this.game.renderer.height*.2,"Controls", {fontSize: 64, color: "#000000"}).setDepth(2); //title
+        this.add.text(this.game.renderer.width*.2, this.game.renderer.height*.3,"Movement:\nW - Move up\nA - Move left\nS - Move down\nD - Move right", {fontSize: 32, color: "#000000"}).setDepth(2); //walking
+        this.add.text(this.game.renderer.width*.4, this.game.renderer.height*.3,"Attacking:\nLeft click - Regular attack\nZ - Special attack", {fontSize: 32, color: "#000000"}).setDepth(2); 
+        this.add.text(this.game.renderer.width*.2, this.game.renderer.height*.5,"Other:\nEsc - Pause", {fontSize: 32, color: "#000000"}).setDepth(2); 
+        controlsBox.alpha = .75
 
         //add button events
         backButton.setInteractive();
