de0eb0dd424ea681aa9335eb3787d54e769001a7
dumdo <43555901+dumdo@users.noreply.github.com>
2019-04-15 21:18:13 -0400

    added timer

--- benchmark2/js/scenes/DayOverlay.js
+++ benchmark2/js/scenes/DayOverlay.js
@@ -13,16 +13,25 @@ export class DayOverlayScene extends Phaser.Scene{
         this.hearts = [];
         this.moneyText;
         this.checkIfMoneyIsSame = 0;
+        this.timer = 10;
+        this.initTime = 0; 
         console.log("entered help");
     }
     create(){
-
         //add images
         //let logo = this.add.image(this.game.renderer.width / 2, this.game.renderer.height*.35, "logo").setDepth(1).setScale(.5,.5);
         for(var i = 0; i < this.hero.health; i++){
             this.hearts.push(this.add.image(50 + i*75,100, "heart").setScale(2,2).setDepth(1));
         }
         this.add.image(1300,100,"coin").setScale(1.2,1.2).setDepth(1);
+        
+        //add timer
+        this.add.image(150, 200, "blueBar").setScale(.5,2).setDepth(1);
+        let seconds = this.timer % 60;
+        if(seconds < 10){
+            seconds = "0" + seconds;
+        }
+        this.timerText = this.add.text(150, 20, Math.floor(this.timer/60) + ':' + seconds, { fontSize: '70px', fill: '#fff', strokeThickness: 10, stroke:"#000000"});
 
         //variables
         this.health = this.hero.health;
@@ -34,6 +43,9 @@ export class DayOverlayScene extends Phaser.Scene{
         //add keyboard keys
     }
     update(time, delta){
+        if(this.initTime == 0){
+            this.initTime = this.time.now;
+        }
         console.log(this.hero.health);
         if(this.hero.health > this.health){
             this.hearts.push(this.add.image(50 + this.health*75,100, "heart").setScale(2,2).setDepth(1));
@@ -52,6 +64,19 @@ export class DayOverlayScene extends Phaser.Scene{
         }
         else{
 
+        }
+        //console.log(this.time.now);
+        //console.log(time);
+        let timeRemaining = this.timer - (Math.floor(time/1000)-Math.floor(this.initTime/1000));
+        let seconds = timeRemaining % 60;
+        if(seconds < 10){
+            seconds = "0" + seconds;
+        }
+        this.timerText.setText(Math.floor(timeRemaining/60) + ":" + seconds);
+        if(timeRemaining <= 0){
+            this.scene.stop(SCENES.DAY);
+            this.scene.start(SCENES.NIGHT,{"level":1,"money":this.hero.money+200});
+            this.scene.stop();
         }
     }
 
