98fea0bfe48c001c3d6af32e5a927766a1315a9f
songdavid98 <songdavid98@gmail.com>
2019-05-15 16:37:59 -0400

    finished night

--- final/js/scenes/nightScene.js
+++ final/js/scenes/nightScene.js
@@ -207,6 +207,8 @@ export class NightScene extends Phaser.Scene {
         buyicetower.setInteractive();
         buyicetower.on("pointerdown", (pointer) => {
             console.log("buyicetower pressed");
+            buycannon.alpha = 1;
+            buyturret.alpha = 1;
             if ( this.money < 400 )
                 return;
             if (!this.alreadyClicked && !this.startDragging) {
@@ -233,6 +235,8 @@ export class NightScene extends Phaser.Scene {
                 this.towerSpriteForBuying = null;
                 this.startDragging = false;
                 this.towerToBePlaced = null;
+                this.rangeCircle.x = -1000;
+                this.rangeCircle.y = -1000;
             }
         });
         buyicetower.on("pointerover", (pointer) => {
@@ -249,12 +253,14 @@ export class NightScene extends Phaser.Scene {
         buycannon.setInteractive();
         buycannon.on("pointerdown", (pointer) => {
             console.log("buycannon pressed");
+            buyicetower.alpha = 1;
+            buyturret.alpha = 1;
             if ( this.money < 100 )
                 return;
             if (!this.alreadyClicked && !this.startDragging) {
                 this.alreadyClicked = true;
                 buycannon.alpha = 0.5;
-                this.towerSpriteForBuying = this.physics.add.sprite(pointer.x, pointer.y, DEFSTR.CANNON, 'right/0003.png').setScale(4, 4);
+                this.towerSpriteForBuying = this.physics.add.sprite(pointer.x, pointer.y, DEFSTR.CANNON, 'right/0003.png').setScale(6, 6);
                 
                 this.towerToBePlaced = new Cannon({
                     "sprite": this.towerSpriteForBuying,
@@ -282,6 +288,8 @@ export class NightScene extends Phaser.Scene {
                 this.towerSpriteForBuying = null;
                 this.startDragging = false;
                 this.towerToBePlaced = null;
+                this.rangeCircle.x = -1000;
+                this.rangeCircle.y = -1000;
             }
         });
         buycannon.on("pointerover", (pointer) => {
@@ -298,6 +306,8 @@ export class NightScene extends Phaser.Scene {
         buyturret.setInteractive();
         buyturret.on("pointerdown", (pointer) => {
             console.log("buyturret pressed");
+            buyicetower.alpha = 1;
+            buycannon.alpha = 1;
             if ( this.money < 100 )
                 return;
             if (!this.alreadyClicked && !this.startDragging) {
@@ -331,6 +341,8 @@ export class NightScene extends Phaser.Scene {
                 this.towerSpriteForBuying = null;
                 this.startDragging = false;
                 this.towerToBePlaced = null;
+                this.rangeCircle.x = -1000;
+                this.rangeCircle.y = -1000;
             }
         });
         buyturret.on("pointerover", (pointer) => {
